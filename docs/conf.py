53c0d60bdaf01cece0cf6243b39d07683dfbdd7d
maciejskorski <maciej.skorski@gmail.com>
2022-09-17 09:41:34 +0000

    setting up sh pages

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+"""Sphinx configuration."""
+
+from cgitb import enable
+import os
+import sys
+
+# General information about the project.
+project = "ML Code Examples"
+author = "Maciej Skorski"
+copyright = "2018-2022, Maciej Skorski"
+
+# Sources
+source_suffix = ['.md']
+master_doc = 'index'
+
+# Sphinx Extensions
+extensions = [
+    "myst_nb",
+    "sphinxcontrib.bibtex",
+]
+bibtex_bibfiles = ['citations.bib']
+
+# html styles
+html_theme = "sphinx_book_theme"
+html_theme_options = {
+    "repository_url": "https://github.com/maciejskorski/ml_examples/",
+    "repository_branch": "master",
+    "path_to_docs": "docs",
+}
+
+nb_execution_mode = "off"
---
+++ docs/index.md
@@ -0,0 +1,4 @@
+---
+maxdepth: 2
+---
+Effectively Monitoring Azure Jobs <../AzureJobs_Dashboard.ipynb>
